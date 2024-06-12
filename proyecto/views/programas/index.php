5cfca4ea541fd50832ed5c6e0dc3b33910f78381
ElRusnder <daniel.ames@unmsm.edu.pe>
2024-06-11 23:45:37 -0500

    Se añade kartik a la tabla programas.

--- proyecto/views/programas/index.php
+++ proyecto/views/programas/index.php
@@ -1,10 +1,12 @@
 <?php
 
 use app\models\Programas;
+use kartik\icons\Icon;
 use yii\helpers\Html;
-use yii\helpers\Url;
+use kartik\grid\GridView;
+use yii\widgets\Pjax;
 use yii\grid\ActionColumn;
-use yii\grid\GridView;
+use Yii\helpers\Url;
 
 /** @var yii\web\View $this */
 /** @var app\models\ProgramasSearch $searchModel */
@@ -13,6 +15,7 @@ use yii\grid\GridView;
 $this->title = 'Programas';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
 <div class="programas-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
@@ -21,11 +24,11 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Registrar Programa', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <!-- Eliminado el render del _search -->
 
     <?= GridView::widget([
+        'id' => 'kv-grid-demo', // Añadido el 'id'
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
@@ -41,10 +44,27 @@ $this->params['breadcrumbs'][] = $this->title;
                 'class' => ActionColumn::className(),
                 'urlCreator' => function ($action, Programas $model, $key, $index, $column) {
                     return Url::toRoute([$action, 'id' => $model->id]);
-                 }
+                }
             ],
         ],
+        'headerContainer' => ['style' => 'top:50px', 'class' => 'kv-table-header'], // offset from top
+        'floatHeader' => true, // table header floats when you scroll
+        'floatPageSummary' => true, // table page summary floats when you scroll
+        'floatFooter' => false, // disable floating of table footer
+        'pjax' => false, // pjax is set to always false for this demo
+        'responsive' => false,
+        'bordered' => true,
+        'striped' => false,
+        'condensed' => true,
+        'hover' => true,
+        'showPageSummary' => true,
+        'panel' => [
+            'heading' => '<i class="fas fa-book"></i> Programa',
+            'type' => 'primary',
+            'before' => '<div style="padding-top: 7px;"></div>',
+        ],
+        'toolbar' => [], // Sin elementos de barra de herramientas
+        'export' => [], // Sin opciones de exportación
     ]); ?>
-
 
 </div>
